1698bdd18e37d6e1c761fe4a4933c30d7c5d6d7f
pythonlearner1025 <mjsong2021@gmail.com>
2023-09-03 18:07:34 -0400

    install ffmpeg & ffprobe

--- backend/test.py
+++ backend/test.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import requests
 
 def test_transcribe_api(path):
-    url = "http://localhost:3000/api/v1/transcribe"
+    url = "https://deforum.dev/api/v1/transcribe"
 
     with open(path, 'rb') as f:
         files = {'file': f}
@@ -13,8 +13,8 @@ def test_transcribe_api(path):
     print(response.json())
 
 if __name__ == "__main__":
-    f = '/Users/minjunes/ghost/audios/Beyond Vector Search： Knowledge Management with Generative AI.m4a'
-    f = '/Users/minjunes/Downloads/Choosing Writing as a Career.m4a'
+    f = '/Users/minjunes/ghost/audios/ex.m4a'
+    #f = '/Users/minjunes/Downloads/Choosing Writing as a Career.m4a'
     test_transcribe_api(f)
 
     
